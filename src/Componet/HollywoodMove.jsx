1e84df8d8615da41e998432c76b6d5802b966a21
pradipkanzareya <pradippradip26808@gamil.com>
2024-08-04 10:38:13 -1200

    added

--- src/Componet/HollywoodMove.jsx
+++ src/Componet/HollywoodMove.jsx
@@ -8,8 +8,8 @@ const HMovie1 = () =>{
         <ReactPlayer
           className='react-player1'
           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
-          height={593}
-          width={1158}
+          height={auto}
+          width={auto}
           controls
         />
     </div>
