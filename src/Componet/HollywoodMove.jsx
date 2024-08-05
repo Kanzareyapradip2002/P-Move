7cc8d1fad01e521ce8a88ff5e505e3a624c45a25
pradipkanzareya <pradippradip26808@gamil.com>
2024-08-04 14:57:51 -1200

    add

--- src/Componet/HollywoodMove.jsx
+++ src/Componet/HollywoodMove.jsx
@@ -127,7 +127,7 @@ const HMovie2 = () =>{
          <ReactPlayer
            className='react-player5'
            url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
-           height={260}
+           height={321}
            width={766}
            controls
          />
