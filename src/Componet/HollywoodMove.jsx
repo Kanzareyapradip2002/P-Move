4c25adb321664bac8a741c018a300ce6026eccdb
pradipkanzareya <pradippradip26808@gamil.com>
2024-08-04 14:48:20 -1200

    Movelinkupdat

--- src/Componet/HollywoodMove.jsx
+++ src/Componet/HollywoodMove.jsx
@@ -86,7 +86,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper'>
          <ReactPlayer
            className='react-player1'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={593}
            width={1158}
            controls
@@ -96,7 +96,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper1'>
          <ReactPlayer
            className='react-player2'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={260}
            width={320}
            controls
@@ -106,7 +106,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper2'>
          <ReactPlayer
            className='react-player3'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={260}
            width={374}
            controls
@@ -116,7 +116,7 @@ const HMovie2 = () =>{
       <div className='player-wrapper3'>
          <ReactPlayer
            className='react-player4'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={260}
            width={424}
            controls
@@ -126,7 +126,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper4'>
          <ReactPlayer
            className='react-player5'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={260}
            width={766}
            controls
@@ -136,7 +136,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper5'>
          <ReactPlayer
            className='react-player6'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={428}
            width={1024}
            controls
@@ -146,7 +146,7 @@ const HMovie2 = () =>{
      <div className='player-wrapper6'>
          <ReactPlayer
            className='react-player7'
-           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Black%20Adam%202022%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas.mkv?alt=media&token=3853357f-efb3-4fc4-9481-6d7d5c0f6d92'
+           url='https://firebasestorage.googleapis.com/v0/b/p-movie-b4834.appspot.com/o/Ghost%20Rider%202%20Spirit%20Of%20Vengeance%202011%20BluRay%20720p%20Hindi%20English%20AAC%205.1%20x264%20ESub%20-%20mkvCinemas%20%5BTelly%5D.mkv?alt=media&token=0cc609ed-6e0d-4049-8e66-c592dcdea994'
            height={604}
            width={1438}
            controls
